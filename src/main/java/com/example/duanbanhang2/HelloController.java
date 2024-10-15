e82117ed5f76dda5be3d47ef1096b307a1820702
vandan <levandanhh2@gmail.com>
2024-10-16 00:42:18 +0700

    chucnangketnoidatabase

--- src/main/java/com/example/duanbanhang2/HelloController.java
+++ src/main/java/com/example/duanbanhang2/HelloController.java
@@ -1,5 +1,19 @@
 package com.example.duanbanhang2;
 
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class HelloController {
+ private final String URL = "jdbc:mysql://localhost:3306/duanbanhang";
+ private final String USERNAME = "root";
+ private final String PASSWORD = "882005";
+
+ public Connection connection() throws SQLException {
+     Connection conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+     System.out.println("Da ket noi thanh cong");
+     return conn;
+ }
 
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.duanbanhang2 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens com.example.duanbanhang2 to javafx.fxml;
