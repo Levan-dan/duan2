ebc3b8c839c153443f2024b3b1770370a2cf9b84
vandan <levandanhh2@gmail.com>
2024-10-17 02:06:39 +0700

    luuthaydoi

--- src/main/java/com/example/duanbanhang2/HelloApplication.java
+++ src/main/java/com/example/duanbanhang2/HelloApplication.java
@@ -1,11 +1,25 @@
 package com.example.duanbanhang2;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 import java.sql.SQLException;
 
-public class HelloApplication {
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+        primaryStage.setTitle("login");
+        primaryStage.setScene(new Scene(root, 400,400));
+        primaryStage.show();
+    }
     public static void main(String[] args) throws SQLException {
-        HelloController helloController = new HelloController();
-        helloController.connection();
+
+       launch(args);
 
     }
 }
--- src/main/java/com/example/duanbanhang2/HelloController.java
+++ src/main/java/com/example/duanbanhang2/HelloController.java
@@ -1,19 +1,26 @@
 package com.example.duanbanhang2;
 
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class HelloController {
- private final String URL = "jdbc:mysql://localhost:3306/duanbanhang";
- private final String USERNAME = "root";
- private final String PASSWORD = "882005";
+    //    chức năng kết nối database
+    private final String URL = "jdbc:mysql://localhost:3306/duanbanhang";
+    private final String USERNAME = "root";
+    private final String PASSWORD = "882005";
 
- public Connection connection() throws SQLException {
-     Connection conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-     System.out.println("Da ket noi thanh cong");
-     return conn;
- }
+    public Connection connection() throws SQLException {
+        Connection conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        System.out.println("Da ket noi thanh cong");
+        return conn;
+    }
+
+
 
 }
