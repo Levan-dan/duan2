2db6a545b484174e1735641ccfb7512dd1e89538
Viet Tam <viettam205@gmail.com>
2024-10-16 16:26:04 +0700

    giaodienlogin

--- src/main/java/com/example/duanbanhang2/HelloApplication.java
+++ src/main/java/com/example/duanbanhang2/HelloApplication.java
@@ -1,11 +1,28 @@
 package com.example.duanbanhang2;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane;
+import java.io.IOException;
 import java.sql.SQLException;
 
-public class HelloApplication {
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+     primaryStage.setTitle("login");
+     primaryStage.setScene(new Scene(root));
+     primaryStage.setMaximized(true);
+     primaryStage.show();
+    }
+
     public static void main(String[] args) throws SQLException {
-        HelloController helloController = new HelloController();
-        helloController.connection();
+
+        launch(args);
 
     }
 }
